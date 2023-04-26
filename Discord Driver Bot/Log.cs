ab3eb4becb48308e27a30a70651ab5527f61ef99
孤之界 <f20001012@gmail.com>
2023-04-26 11:08:21 +0800

    add: 忽略 Clyde 造成的錯誤訊息

--- Discord Driver Bot/Log.cs
+++ Discord Driver Bot/Log.cs
@@ -78,9 +78,12 @@ public static class Log
         if (!string.IsNullOrEmpty(message.Message)) FormatColorWrite(message.Message, consoleColor);
 #endif
 
-        if (message.Exception != null && message.Exception is not GatewayReconnectException)
+        if (message.Exception != null && message.Message != null && !message.Message.Contains("TYPING_START") && (message.Exception is not GatewayReconnectException || message.Exception is not TaskCanceledException))
         {
             consoleColor = ConsoleColor.DarkRed;
+#if RELEASE
+            FormatColorWrite(message.Message, consoleColor);
+#endif
             FormatColorWrite(message.Exception.GetType().FullName, consoleColor);
             FormatColorWrite(message.Exception.Message, consoleColor);
             FormatColorWrite(message.Exception.StackTrace, consoleColor);

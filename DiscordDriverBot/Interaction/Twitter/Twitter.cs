ca299b6d0da04c454ee26092869cec123c66c35b
孤之界 <f20001012@gmail.com>
2023-10-16 11:35:09 +0800

    feature: 透過斜線指令來轉換網址

--- DiscordDriverBot/Interaction/Twitter/Twitter.cs
+++ DiscordDriverBot/Interaction/Twitter/Twitter.cs
@@ -6,8 +6,15 @@ namespace DiscordDriverBot.Interaction.Twitter
 {
     public class Twitter : TopLevelModule
     {
+        [SlashCommand("convert-to-vxtwitter", "將網址轉換成 vxTwitter")]
+        public async Task ConvertToVxTwitter([Summary("url", "網址")]string url)
+        {
+            var fixedUrl = url.Replace("twitter.com", "vxtwitter.com").Replace("x.com", "fixvx.com");
+            await Context.Interaction.RespondAsync(fixedUrl);
+        }
+
         [MessageCommand("轉換網址成 vxTwitter")]
-        public async Task ConvertToVxTwitter(IMessage message)
+        public async Task ConvertMessageToVxTwitter(IMessage message)
         {
             await Context.Interaction.SendConfirmAsync("Working...", false, true);
 

f8f1ffd646813a4c5a5b971ab262704fda7c7408
孤之界 <f20001012@gmail.com>
2023-10-18 18:15:18 +0800

    feature: 改成直接回應已修正的訊息

--- DiscordDriverBot/Interaction/Twitter/Twitter.cs
+++ DiscordDriverBot/Interaction/Twitter/Twitter.cs
@@ -10,17 +10,14 @@ namespace DiscordDriverBot.Interaction.Twitter
         public async Task ConvertToVxTwitter([Summary("url", "網址")]string url)
         {
             var fixedUrl = url.Replace("twitter.com", "vxtwitter.com").Replace("x.com", "fixvx.com");
-            await Context.Interaction.RespondAsync(fixedUrl);
+            await Context.Interaction.RespondAsync(fixedUrl, allowedMentions: AllowedMentions.None);
         }
 
         [MessageCommand("轉換網址成 vxTwitter")]
         public async Task ConvertMessageToVxTwitter(IMessage message)
         {
-            await Context.Interaction.SendConfirmAsync("Working...", false, true);
-
             var fixedMessage = message.Content.Replace("twitter.com", "vxtwitter.com").Replace("x.com", "fixvx.com");
-
-            await Context.Channel.SendMessageAsync(fixedMessage, allowedMentions: AllowedMentions.None);
+            await Context.Interaction.RespondAsync(fixedMessage, allowedMentions: AllowedMentions.None);
         }
     }
 }
